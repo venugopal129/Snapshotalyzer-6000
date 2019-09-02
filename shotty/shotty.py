b788e5fa9bb6bfa4a04c85d02eb799a7129ce56c
venugopal129 <venugopalreddy.aleti@gmail.com>
2019-09-02 21:48:00 +0200

    click imported and modified function

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,9 +1,16 @@
 import boto3
+import click
+session=boto3.Session(profile_name='shotty')
+ec2=session.resource('ec2')
+@click.command()
+def list_instances():
+    "List EC2 Instances"
+    for i in ec2.instances.all():
+        print(','.join((i.id,
+            i.instance_type,
+            i.state['Name'])))
+
+    return
 
 if __name__=='__main__':
-
-    session=boto3.Session(profile_name='shotty')
-    ec2=session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
